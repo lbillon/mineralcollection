8e7e997822c69d8d786dda96b59f7d5d79891a33
Laurent Billon <laurent.billonc@gmail.com>
2014-11-17 11:12:27 +0100

    Revert commited personal configuration

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'mineral_db';
+$db['default']['username'] = 'mineraldb';
+$db['default']['password'] = 'mineraldb';
+$db['default']['database'] = 'mineraldb';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
